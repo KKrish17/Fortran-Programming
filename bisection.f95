5df7aa9ea917424aa1f41c0788a9da4d8bb3a983
Krishnagopal Kar <43494227+KKrish17@users.noreply.github.com>
2020-09-11 19:32:26 +0530

    Update bisection.f95

--- bisection.f95
+++ bisection.f95
@@ -2,15 +2,15 @@ program bisection
  implicit none 
  real,external::f 
  real::a,b,c 
- write(*,*)'Enter the value of a and b such that f(a) and f(b) should have opposite sign'
+ write(*,*)'Enter the value of a and b (First and second approximation in which the root lies):'
  read(*,*)a,b 
  if(f(a)*f(b).gt.0) then 
   write(*,*)'Enter a and b value correctly'
  else 
   10 c=(a+b)/2
     if(f(c).eq.0) then 
-     write(*,20)'The roots of the equation upto 4 decimal place is :',c
-     20 format (A,F12.4)
+     write(*,20)'The roots of the equation upto 6 decimal place is :',c
+     20 format (A,F12.6)
     else if (f(a)*f(c).gt.0) then 
      a=c 
      goto 10 
@@ -23,5 +23,5 @@ end
 
 real function f(x)
  real::x
- f=(3*x)-1 
+ f=(3*x)-1
 end 
